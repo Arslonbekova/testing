901bfc96d2f0002a72e175c47fb6ab395fc3b436
Arslonbekova <16halo03@gmail.com>
2023-09-26 15:06:08 +0500

    finished

---
+++ set-map.js
@@ -0,0 +1,340 @@
+// let obj=new Object();
+// name={};
+// surname={};
+
+// let  s=new Date();
+// for (let i=0;i<100000;i++){
+//     let doSomthing=i*i*i*i*i*i;
+// }
+// let end=new Date();
+// console.log(`fgcvhbjnkm ${end-s} bnm`);
+
+
+
+
+
+// let lest=["8+9=? yig'indini hisoblang \n a)17 \n b)2 \n c)5 \n d)9",
+// "8+10=? yig'indini hisoblang \n a)18 \n b)2 \n c)5 \n d)9",
+// "8+11=? yig'indini hisoblang \n a)17 \n b)18 \n c)19 \n d)9",
+// "8+12=? yig'indini hisoblang \n a)17 \n b)2 \n c)8 \n d)20",
+// "8+13=? yig'indini hisoblang \n a)17 \n b)2 \n c)5 \n d)21",
+
+// "8+14=? yig'indini hisoblang \n a)22 \n b)2 \n c)5 \n d)9",
+// "8+15=? yig'indini hisoblang \n a)18 \n b)2 \n c)23 \n d)9",
+// "8+16=? yig'indini hisoblang \n a)17 \n b)24 \n c)19 \n d)9",
+// "8+17=? yig'indini hisoblang \n a)25 \n b)2 \n c)8 \n d)20",
+// "8+18=? yig'indini hisoblang \n a)17 \n b)2 \n c)5 \n d)26"
+// ]
+// let ILTS=["fan yoki sport olimpiadalarida faxrli o'rin egallaganmi(oxirgi ikki yil ichida) \n a)ha \n b)yo'q ",
+// "chet tili sertifikatingiz mavjudmi \n a)ha \n b)yo'q ",
+// "nogironligi to'g'risidagi xujjat mavjudmi \n a)ha \n b)yo'q"]
+
+// // function kvadrat(a,qiymat){
+// //     qiymat="";
+// //     qiymat=qiymat+"     "+(a+1)+"."+qiymat[a]; 
+// //     return qiymat;
+// // }
+// // 
+// // 
+
+// let d=0;
+// let kiritilgan_javoblar="",javoblar="";
+// let javob=["a","a","c","d","d","a","c","b","a","d"];
+// for(let i=0;i<=lest.length-1;i++){
+//     variant=prompt(lest[i]);
+//     if((variant[0]).toLowerCase()==javob[i]){
+//         d=d+5;
+//     }
+//     else{
+//         d=d+0;
+//     }
+
+//     javoblar=javoblar+"     "+(i+1)+"."+javob[i];
+//     kiritilgan_javoblar=kiritilgan_javoblar+"     "+(i+1)+"."+variant[0].toLowerCase();
+
+// }
+// let tjavob=lest.length-(lest.length-(d/5));
+// let samaradorlik=((tjavob*100)/(lest.length));
+// alert(`${lest.length}savoldan ${tjavob}tasiga to'gri javob berdingiz \n ${(lest.length-(lest.length-(d/5)))*5} bal to'pladingiz \n samaradorlik ${samaradorlik}% \n javoblar ketma-ketligi quyidagicha: \n ${javoblar}  \n sizning javoblaringiz ketma-ketligi quyidagicha:\n${kiritilgan_javoblar} `)
+
+
+
+
+// let lest=[
+//     {
+//         savol:["8+9=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.2" , "c.5" ,"d.9"],
+//         javob:["a","17"]
+//     },
+//     {
+//         savol:["8+10=? yig'indini hisoblang"],
+//         variant:[ "a.17", "b.18" , "c.5" ,"d.9"],
+//         javob:["b","18"]
+//     },
+//     {
+//         savol:["8+11=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.2" , "c.19" ,"d.9"],
+//         javob:["c","19"]
+//     },
+//     {
+//         savol:["8+12=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.2" , "c.20" ,"d.9"],
+//         javob:["c","20"]
+//     },
+//     {
+//         savol:["8+13=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.2" , "c.21" ,"d.9"],
+//         javob:["c","21"]
+//     },
+//     {
+//         savol:["8+14=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.22" , "c.5" ,"d.9"],
+//         javob:["b","22"]
+//     },
+//     {
+//         savol:["8+15=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.23" , "c.5" ,"d.9"],
+//         javob:["b","23"]
+//     },
+//     {
+//         savol:["8+16=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.2" , "c.24" ,"d.9"],
+//         javob:["c","24"]
+//     },
+//     {
+//         savol:["8+17=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.25" , "c.5" ,"d.9"],
+//         javob:["b","25"]
+//     },
+//     {
+//         savol:["8+18=? yig'indini hisoblang"],
+//         variant:[ "a.17",  "b.2" , "c.26" ,"d.9"],
+//         javob:["c","26"]
+//     }
+//     // {
+//     //     savol:["8+19=? yig'indini hisoblang"],
+//     //     variant:[ "a.27",  "b.2" , "c.5" ,"d.9"],
+//     //     javob:["a","27"]
+//     // },
+//     // {
+//     //     savol:["8+20=? yig'indini hisoblang"],
+//     //     variant:[ "a.28",  "b.2" , "c.5" ,"d.9"],
+//     //     javob:["a","28"]
+//     // }
+//     ];
+
+// function startTest(lest){
+ 
+
+// let ILTS=["fan yoki sport olimpiadalarida faxrli o'rin egallaganmi(oxirgi ikki yil ichida) \n a)ha \n b)yo'q ",
+// "chet tili sertifikatingiz mavjudmi \n a)ha \n b)yo'q ",
+// "nogironligi to'g'risidagi xujjat mavjudmi \n a)ha \n b)yo'q"]
+
+// let b=0,b1=0,b2=0;
+// let imtiyozlar=[];
+// for(let j=0;j<=ILTS.length-1;j++){
+//     imtiyozlar.push(variant=prompt(ILTS[j]));
+// }
+//     if((imtiyozlar[0]).toLowerCase()=="a"){
+//         b=b+50;
+//     }
+//     if((imtiyozlar[1]).toLowerCase()=="a"){
+//         b1=b1+25;
+//     }
+//     if((imtiyozlar[2]).toLowerCase()=="a"){
+//         b2=2;
+//     }
+
+// let d=0;
+// let kiritilgan_javoblar="",javoblar="";
+
+
+// for(let i=0;i<=lest.length-1;i++){
+//     variant=prompt(` ${lest[i].savol} \n ${lest[i].variant[0]}\n ${lest[i].variant[1]}\n ${lest[i].variant[2]} \n ${lest[i].variant[3]}`);
+//     if((variant[0]==(lest[i].javob[0]))||(variant.toLowerCase()==(lest[i].javob[1]))){
+//         d=d+5;
+//     }
+//     else{
+//         d=d+0;
+//     }
+//     if(variant.toLowerCase()==("quet"||"chiqish"||"break")){
+//         i=lest.length;
+//     }
+//     javoblar=javoblar+"     "+(i+1)+"."+lest[i].javob;  
+//     kiritilgan_javoblar=kiritilgan_javoblar+"     "+(i+1)+"."+variant;
+
+// }
+// let tjavob=lest.length-(lest.length-(d/5));
+// let samaradorlik=((tjavob*100)/(lest.length));
+// alert(`${lest.length}savoldan ${tjavob}tasiga to'gri javob berdingiz \n testdan  ${(lest.length-(lest.length-(d/5)))*5} bal to'pladingiz \n ${(((lest.length-(lest.length-(d/5)))*5)/b)+b+b1} imtiyoz balingiz\n ${(b+b1+(((lest.length-(lest.length-(d/5)))*5)/b))+((lest.length-(lest.length-(d/5)))*5)} umumiy bal to'pladingiz \n samaradorlik ${samaradorlik}% \n javoblar ketma-ketligi quyidagicha: \n ${javoblar}  \n sizning javoblaringiz ketma-ketligi quyidagicha:\n ${kiritilgan_javoblar} `)
+
+// }
+// startTest(lest);
+
+
+
+///////////verstion(0.3)
+let lest=[
+    {
+        savol:["8+9=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.2" , "c.5" ,"d.9"],
+        javob:["a","17"]
+    },
+    {
+        savol:["8+10=? yig'indini hisoblang"],
+        variant:[ "a.17", "b.18" , "c.5" ,"d.9"],
+        javob:["b","18"]
+    },
+    {
+        savol:["8+11=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.2" , "c.19" ,"d.9"],
+        javob:["c","19"]
+    },
+    {
+        savol:["8+12=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.2" , "c.20" ,"d.9"],
+        javob:["c","20"]
+    },
+    {
+        savol:["8+13=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.2" , "c.21" ,"d.9"],
+        javob:["c","21"]
+    },
+    {
+        savol:["8+14=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.22" , "c.5" ,"d.9"],
+        javob:["b","22"]
+    },
+    {
+        savol:["8+15=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.23" , "c.5" ,"d.9"],
+        javob:["b","23"]
+    },
+    {
+        savol:["8+16=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.2" , "c.24" ,"d.9"],
+        javob:["c","24"]
+    },
+    {
+        savol:["8+17=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.25" , "c.5" ,"d.9"],
+        javob:["b","25"]
+    },
+    {
+        savol:["8+18=? yig'indini hisoblang"],
+        variant:[ "a.17",  "b.2" , "c.26" ,"d.9"],
+        javob:["c","26"]
+    }
+    // {
+    //     savol:["8+19=? yig'indini hisoblang"],
+    //     variant:[ "a.27",  "b.2" , "c.5" ,"d.9"],
+    //     javob:["a","27"]
+    // },
+    // {
+    //     savol:["8+20=? yig'indini hisoblang"],
+    //     variant:[ "a.28",  "b.2" , "c.5" ,"d.9"],
+    //     javob:["a","28"]
+    // }
+    ];
+function startTest() {
+    let x=1;
+    while (x) {
+        let tanlov=(prompt(`tanlang \n a)admen \n b)user`)).toLowerCase();
+            if((tanlov=="admen")||(tanlov=="a")){
+               admen();
+            }
+            if((tanlov=="user")||(tanlov=="b")){
+                user(lest);
+            }
+            let tekshirish=prompt(`test ishlab ko'rishni istaysizmi \n a)ha \n b)yo'q`);
+            if((tekshirish=="a")||(tekshirish=="ha")){
+                x=1;
+            }
+            else{
+                x=0; 
+                break;
+            }
+    }  
+}
+startTest();
+
+function user(lest){
+ 
+    let ILTS=["fan yoki sport olimpiadalarida faxrli o'rin egallaganmi(oxirgi ikki yil ichida) \n a)ha \n b)yo'q ",
+    "chet tili sertifikatingiz mavjudmi \n a)ha \n b)yo'q ",
+    "nogironligi to'g'risidagi xujjat mavjudmi \n a)ha \n b)yo'q"]
+    
+    let b=0,b1=0,b2=0;
+    let imtiyozlar=[];
+    for(let j=0;j<=ILTS.length-1;j++){
+        imtiyozlar.push(variant=prompt(ILTS[j]));
+    }
+        if((imtiyozlar[0]).toLowerCase()==("a"||"ha")){
+            b=b+50;
+        }
+        if((imtiyozlar[1]).toLowerCase()=="a"||"ha"){
+            b1=b1+25;
+        }
+        if((imtiyozlar[2]).toLowerCase()=="a"||"ha"){
+            b2=2;
+        }
+    
+    let d=0;
+    let kiritilgan_javoblar="",javoblar="";
+    
+    for(let i=0;i<=lest.length-1;i++){
+        variant=prompt(` ${lest[i].savol} \n ${lest[i].variant[0]}\n ${lest[i].variant[1]}\n ${lest[i].variant[2]} \n ${lest[i].variant[3]}`);
+        if((variant[0]==(lest[i].javob[0]))||(variant.toLowerCase()==(lest[i].javob[1]))){
+            d=d+5;
+        }
+        else{
+            d=d+0;
+        }
+        if(variant.toLowerCase()==("quet"||"chiqish"||"break")){
+            i=lest.length;
+        }
+        javoblar=javoblar+"     "+(i+1)+"."+lest[i].javob;  
+        kiritilgan_javoblar=kiritilgan_javoblar+"     "+(i+1)+"."+variant;
+    
+    }
+    let tjavob=lest.length-(lest.length-(d/5));
+    let samaradorlik=((tjavob*100)/(lest.length));
+    alert(`${lest.length}savoldan ${tjavob}tasiga to'gri javob berdingiz \n testdan  ${(lest.length-(lest.length-(d/5)))*5} bal to'pladingiz \n ${(((lest.length-(lest.length-(d/5)))*5)/b)+b+b1} imtiyoz balingiz\n ${(b+b1+(((lest.length-(lest.length-(d/5)))*5)/b))+((lest.length-(lest.length-(d/5)))*5)} umumiy bal to'pladingiz \n samaradorlik ${samaradorlik}% \n javoblar ketma-ketligi quyidagicha: \n ${javoblar}  \n sizning javoblaringiz ketma-ketligi quyidagicha:\n ${kiritilgan_javoblar} `)
+    
+    }
+function admen() {
+    let b1=1,xatolik=0,parol1="1234";;
+    while (b1) {
+        let parol=prompt(`parolni kiriting`);
+        if(parol==parol1){
+            let savol1=prompt(`a)savol kiritishni istaysizmi \nb)savol taxrirlashni istaysizmi` );
+            if((savol1.toLowerCase()=="a")||(savol1.toLowerCase()=="savol")){
+                let y=1,push1={savol:[] ,variant:[] ,javob:[] };
+                while (y) {
+                    push1.savol.push(prompt(`savolni kiriting`));
+                    for (let i = 0; i <=3; i++) {
+                        push1.variant.push(prompt(`${i+1} variantni kiriting`));  
+                    }
+                    for (let i = 0; i <=1; i++) {
+                        push1.javob.push(prompt(`javob variantini kiriting(qiymatini kiring)`));  
+                    }
+                    console.log(push1);
+                    let yana=prompt(`yana savolni kiritishni istaysizmi \n a)ha \n b)yo'q`);
+                    if((yana=="b")||(yana=="yo'q")){
+                        y=0,b1=0;
+                    }
+                }
+                lest.push(push1);
+            }
+        }
+        else{
+            alert(`parol noto'gri `);
+            xatolik=xatolik+1;
+        } 
+        if(xatolik==3){
+            alert(`siz 3 marta xatom kiritdingiz va sayt tomonidan bloklandingiz`);
+            b1=0;
+            break;
+        }
+    }
+}
